a5b33d91cf32838194f7cd5ea8b258a1e94766c1
hempflower <1993996310@qq.com>
2022-06-22 19:16:41 +0800

    整理前端项目代码

---
+++ front-end/src/components/controls/RButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="r-button" @click="emit('click')">
+    <slot></slot>
+    {{ props.icon }}
+  </div>
+</template>
+<script setup lang="ts">
+
+export interface RButtonProps {
+  icon?: string;
+  outline?: boolean;
+  round?: boolean;
+  color?: string;
+}
+
+
+const props = withDefaults(defineProps<RButtonProps>(), {
+  icon: () => "",
+  outline: () => false,
+  round: () => false,
+  string: () => ""
+});
+
+const emit = defineEmits(["click"]);
+
+</script>
+<style scoped>
+.r-button {
+  padding: 4px 8px;
+  border-radius: 4px;
+}
+</style>
--- front-end/src/views/AboutView.vue
+++ front-end/src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <RButton>testttest</RButton>
   </div>
 </template>
 
@@ -13,3 +14,6 @@
   }
 }
 </style>
+<script setup lang="ts">
+import RButton from "@/components/controls/RButton.vue";
+</script>
