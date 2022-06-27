bd0818ddf83b88268eeedb0d7d32aff907a2af4f
lc6464 <64722907+lc6464@users.noreply.github.com>
2022-06-27 10:35:44 +0800

    更改数据表结构

---
+++ back-end/Database/Tables/OAuth.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkinServerNext.Database.Tables;
+public class OAuthAccount {
+	public int Id { get; set; }
+	public int UserId { get; set; }
+	public string? MicrosoftId { get; set; }
+	public string? MicrosoftName { get; set; }
+	public string? MicrosoftDisplayName { get; set; }
+
+	public OAuthAccount(int userId, string? microsoftId = null, string? microsoftName = null, string? microsoftDisplayName = null) {
+		UserId = userId;
+		MicrosoftId = microsoftId;
+		MicrosoftName = microsoftName;
+		MicrosoftDisplayName = microsoftDisplayName;
+	}
+}
--- back-end/Database/Tables/User.cs
+++ back-end/Database/Tables/User.cs
@@ -2,27 +2,31 @@
 
 namespace SkinServerNext.Database.Tables;
 public class User {
-	public long Id { get; set; }
+	public int Id { get; set; }
 
-	[Required]
-	public string? Name { get; set; }
+	[StringLength(128)]
+	public string Name { get; set; }
 
-	[Required]
+	[StringLength(128)]
 	public string? DisplayName { get; set; }
 
-	[Required]
-	public string? PasswordSalt { get; set; }
+	[StringLength(64)]
+	public string PasswordSalt { get; set; }
 
-	[Required]
-	public string? PasswordHash { get; set; }
+	[StringLength(44)]
+	public string PasswordHash { get; set; }
 
-	[Required]
-	public string? Email { get; set; }
+	[StringLength(128)]
+	public string Email { get; set; }
 
-	[Required]
-	public int Level { get; set; }
-	
-	public string? MicrosoftID { get; set; }
-	public string? MicrosoftName { get; set; }
-	public string? MicrosoftDisplayName { get; set; }
+	public ulong Experience { get; set; }
+
+	public User(string name, string passwordSalt, string passwordHash, string email, ulong experience, string? displayName = null) {
+		Name = name;
+		PasswordSalt = passwordSalt;
+		PasswordHash = passwordHash;
+		Email = email;
+		Experience = experience;
+		DisplayName = displayName;
+	}
 }
