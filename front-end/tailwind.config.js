d5ad16c05f590ab1f192e14d5573cdd7bb98baa8
hempflower <1993996310@qq.com>
2022-08-04 22:55:51 +0800

    使用 Quasar 作为前端框架

--- front-end/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
